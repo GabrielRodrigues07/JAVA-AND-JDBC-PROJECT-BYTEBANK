bb53d87c74db0e56e07836f869c1929d67bc1938
Gabriel <grodrigues256@gmail.com>
2024-03-03 09:43:03 -0300

    feat(sm3-#03-03-24) Escalando conexoes com o banco

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,17 +1,28 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
     public Connection recuperarConexao() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/bytebank?user=root&password=root");
+            return createDataSource().getConnection();
 
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
         }
+    }
+
+    private HikariDataSource  createDataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/bytebank");
+        hikariConfig.setUsername("root");
+        hikariConfig.setPassword("root");
+        hikariConfig.setMaximumPoolSize(10);
+        return new HikariDataSource(hikariConfig);
     }
 }
